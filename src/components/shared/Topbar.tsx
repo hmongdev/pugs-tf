6a7a6cd5f67da41278032581ac6073d1084ec99c
HmongDev <steveyangwork@gmail.com>
2024-09-13 23:20:19 -0500

    add Steam Login button

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,10 +1,19 @@
 'use client';
 
+import { Button } from '../ui/button';
+
 export default function Topbar() {
+	const handleSteamLogin = () => {
+		alert('Steam Login!');
+	};
+
 	return (
 		<nav className="topbar">
 			<div className="absolute -inset-px bg-gradient-to-r from-orange-700/60 via-red-500 to-yellow-500" aria-hidden="true"></div>
-			<div className="absolute inset-0 bg-card" aria-hidden="true" />
+			<div className="absolute inset-0 bg-card" aria-hidden="false" />
+			<Button className="z-10 text-gray-200" onClick={handleSteamLogin}>
+				Steam Login
+			</Button>
 		</nav>
 	);
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+	'inline-flex items-center justify-center whitespace-nowrap rounded-xl bg-gradient-to-t from-green-900 to-lime-600 text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 	{
 		variants: {
 			variant: {
@@ -17,7 +17,7 @@ const buttonVariants = cva(
 				link: 'text-primary underline-offset-4 hover:underline',
 			},
 			size: {
-				default: 'h-10 px-4 py-1',
+				default: 'h-8 px-2 py-1',
 				sm: 'h-9 rounded-md px-3',
 				lg: 'h-11 rounded-md px-8',
 				icon: 'h-10 w-10',
